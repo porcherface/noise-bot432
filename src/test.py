9676ed993f5d43048b0642872190c305393efe61
andrew <porcherface@gmail.com>
2021-01-04 13:06:31 +0100

    added example to get, ehm, inspiration

---
+++ src/test.py
@@ -0,0 +1,9 @@
+# test.py
+from pyfacebook import Api
+
+APP_ID = "Your APP ID"
+APP_SECRET = "Your APP SECRET"
+ACCESS_TOKEN = "Your Access Token"
+
+if __name__ == "__main__":
+    print("all set")
